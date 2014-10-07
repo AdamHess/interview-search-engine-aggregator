22f7de2b2eb9e710e7f9371083414304d0c00fd1
Adam Hess <TheSpiderPig@users.noreply.github.com>
2014-10-07 15:56:12 -0400

    removed unused dependancies and updated package.json repository

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 'use strict';
 
 var path = require('path'),
-	express = require('express'),
-	hbs = require('hbs'),
-	request = require('request'),
-	select = require('soupselect').select,
-	htmlparser = require('htmlparser'),
-	parserToHtml = require('htmlparser-to-html');
+    express = require('express'),
+    request = require('request'),
+    select = require('soupselect').select,
+    htmlparser = require('htmlparser'),
+    parserToHtml = require('htmlparser-to-html');
 
-	var app = express();
-	//use handlebars as rendering engine 
-	app.set('view engine', 'html');
-	app.engine('html', require('hbs').__express);
+var app = express();
 
-
-	app.get('/', function(req, resp) {
-	    resp.sendfile(path.resolve('./public/index.html'));
-	});
+app.get('/', function(req, resp) {
+    resp.sendfile(path.resolve('./public/index.html'));
+});
 
 
-
-	app.use(express.static(path.resolve('./public')));
+app.use(express.static(path.resolve('./public')));
 
 
 
@@ -28,51 +22,58 @@ var path = require('path'),
 //need to get all requests from various sources 
 
 
- app.get('/query/:searchEngine', function(req, resp) {
+app.get('/query/:searchEngine', function(req, resp) {
     var searchQuery = req.query.query;
     var searchEngine = req.param('searchEngine');
 
-    if ((searchEngine !== 'yahoo') && 
-    	(searchEngine !== 'google') && 
-    	(searchEngine !== 'bing')) {
-    	resp.status(400).send({error: "Invalid Search Engine: Supports only google, yahoo and bing"});
-    	return;
+    if ((searchEngine !== 'yahoo') &&
+        (searchEngine !== 'google') &&
+        (searchEngine !== 'bing')) {
+        resp.status(400).send({
+            error: 'Invalid Search Engine: Supports only google, yahoo and bing'
+        });
+        return;
     }
-    if (searchQuery=== undefined) {
-    	resp.status(400).send({error: "Invalid Request: Must provide search query"});
+    if (searchQuery === undefined) {
+        resp.status(400).send({
+            error: 'Invalid Request: Must provide search query'
+        });
     }
 
     var searchString;
-    var searchSelector
+    var searchSelector;
     if (searchEngine === 'yahoo') {
-    	searchString = 'https://search.yahoo.com/search?q=';
-    	searchSelector = '#main [start=1]';
-    }
- 	else if (searchEngine === 'google') {
-    	searchString = 'https://www.google.com/search?q=';
-    	searchSelector = '.g';
-    }
-     else if (searchEngine === 'bing') {
-    	searchString = 'http://www.bing.com/search?q=';
-    	searchSelector = '#b_results';
+        searchString = 'https://search.yahoo.com/search?q=';
+        searchSelector = '#main [start=1]';
+    } else if (searchEngine === 'google') {
+        searchString = 'https://www.google.com/search?q=';
+        searchSelector = '.g';
+    } else if (searchEngine === 'bing') {
+        searchString = 'http://www.bing.com/search?q=';
+        searchSelector = '#b_results';
     }
 
     request.get(searchString + searchQuery, function(error, response, body) {
-    	if (error) {
-    		resp.status(500).send({error:"Something went wrong getting request from " + searchEngine + ' ' + error});
-    	}
-    	var handler = new htmlparser.DefaultHandler(function(error, dom) {
-    		if (error) {
-		   		resp.status(500).send({error:"something went wrong with parsing response from " + searchEngine + ' ' + error});
-    		}
-    		else {
-				var queries = parserToHtml(select(dom, searchSelector));
-				resp.status(200).send({resp: queries});
-    		}
-    	});
+        if (error) {
+            resp.status(500).send({
+                error: 'Something went wrong getting request from ' + searchEngine + ' ' + error
+            });
+        }
+        var handler = new htmlparser.DefaultHandler(function(error, dom) {
+            if (error) {
+                resp.status(500).send({
+                    error: 'something went wrong with parsing response from ' + searchEngine + ' ' + error
+                });
+            } else {
+                var queries = parserToHtml(select(dom, searchSelector));
+                resp.status(200).send({
+                    resp: queries
+                });
+            }
+        });
 
-    	var parser = new htmlparser.Parser(handler);
-    	parser.parseComplete(body);
+        var parser = new htmlparser.Parser(handler);
+        parser.parseComplete(body);
 
 
     });
@@ -80,7 +81,6 @@ var path = require('path'),
 
 });
 var port = 8080;
- app.listen(port);
+app.listen(port);
 
- console.log("Application running on Port: " + port);
-
+console.log('Application running on Port: ' + port);
