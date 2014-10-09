2f618217692af390491e8877f3e038d478d4124a
Adam Hess <TheSpiderPig@users.noreply.github.com>
2014-10-09 01:18:02 -0400

    Created aggregated query service (one service will hit all 3 providers)

--- server.js
+++ server.js
@@ -3,25 +3,37 @@
 var path = require('path'),
     express = require('express'),
     request = require('request'),
+    async = require('async'),
     select = require('soupselect').select,
     htmlparser = require('htmlparser'),
+    cheerio = require('cheerio'),
     parserToHtml = require('htmlparser-to-html');
+
+
+
+// constants
+
+var queryUrls = {
+    'bing': 'http://www.bing.com/search?q=',
+    'yahoo': 'https://search.yahoo.com/search?q='
+};
+//helper function 
+function generateQueryURL(query, searchEngine) {
+    return queryUrls[searchEngine] + query;
+}
+
+
 
 var app = express();
 
+// file server for everything in the publc directory 
 app.get('/', function(req, resp) {
     resp.sendfile(path.resolve('./public/index.html'));
 });
-
-
 app.use(express.static(path.resolve('./public')));
 
 
-
-//Steps
-//need to get all requests from various sources 
-
-
+//Middle man API for search engines 
 app.get('/query/:searchEngine', function(req, resp) {
     var searchQuery = req.query.query;
     var searchEngine = req.param('searchEngine');
@@ -64,7 +76,9 @@ app.get('/query/:searchEngine', function(req, resp) {
                     error: 'something went wrong with parsing response from ' + searchEngine + ' ' + error
                 });
             } else {
+
                 var queries = parserToHtml(select(dom, searchSelector));
+
                 resp.status(200).send({
                     resp: queries
                 });
@@ -77,9 +91,129 @@ app.get('/query/:searchEngine', function(req, resp) {
 
     });
 
+});
+
+function processGoogleRequest(query, callback) {
+    request.get('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + query,
+        function(error, response, body) {
+            if (error) {
+                callback(error, []);
+                return;
+            }
+            var searchResults = body;
+            if (typeof body !== 'object') {
+                searchResults = JSON.parse(body);
+            }
+
+            var googleResults = [];
+            searchResults.responseData.results.forEach(function(element) {
+                var aResult = {
+                    url: element.url,
+                    title: element.title,
+                    description: element.content
+                };
+                googleResults.push(aResult);
+            });
+            callback(null, googleResults);
+
+        });
+}
+
+
+
+
+
+app.get('/aggregated_query/:query', function(req, resp) {
+    var query = req.param('query');
+
+    //process all requests in parallel and then send response when all is processed
+    async.parallel({
+        google: function(callback) {
+            //use google's api's to generate results 
+            processGoogleRequest(query, callback);
+        },
+        bing: function(callback) {
+            processSearchRequest(generateQueryURL(query, 'bing'), bingProcessingFunction, callback);
+
+        },
+        yahoo: function(callback) {
+            processSearchRequest(generateQueryURL(query, 'yahoo'), yahooProcessingFunction, callback);
+        },
+    }, function(error, results) {
+        if (error) {
+            resp.status(200).send({error: "Error Handling Request: " + error});
+        }
+        else {
+            results.query = query;
+            resp.status(200).send(results);    
+        }
+        
+    });
+
 
 });
-var port = 8080;
+
+//Keep things DRY by writing generic processes function
+//specialized handling is needed on a case by case basis
+
+function processSearchRequest(queryUrl, processingFunction, callback) {
+
+    request.get(queryUrl,
+        function(error, response, body) {
+            if (error) {
+                callback(error, []);
+                return;
+            }
+            var $ = cheerio.load(body);
+            var queryResults = processingFunction($);
+            callback(null, queryResults);
+        });
+}
+
+function yahooProcessingFunction($) {
+
+    var yahooResults = [];
+    $('.res').each(function() {
+        var urlAndTitle = $(this).find('h3 a');
+        var url = urlAndTitle.attr('href');
+        var title = $(urlAndTitle).text();
+        var $desc = $(this).find('.res .abstr');
+        var description = $desc.first().text();
+
+        var aResult = {
+            'url': url,
+            'title': title,
+            'description': description
+        };
+        yahooResults.push(aResult);
+    });
+    return yahooResults;
+}
+
+
+function bingProcessingFunction($) {
+    var bingResults = [];
+    $('.b_algo').each(function() {
+        var urlAndTitle = $(this).find('h2 a');
+        var url = urlAndTitle.attr('href');
+        var title = $(urlAndTitle).text();
+        var $desc = $(this).find('.b_caption p');
+        var description = $desc.first().text();
+
+        var aResult = {
+            'url': url,
+            'title': title,
+            'description': description
+        };
+        bingResults.push(aResult);
+    });
+
+    return bingResults;
+
+}
+
+
+var port = 9000;
 app.listen(port);
 
 console.log('Application running on Port: ' + port);
