97e6cd50d2ea209a8d0f694e53e72615ba5f13f9
Adam Hess <TheSpiderPig@users.noreply.github.com>
2014-10-09 09:42:29 -0400

    Removed old query service (now only aggregated query is supported) updated handlebars template for search results

--- public/main.js
+++ public/main.js
@@ -1,171 +1,34 @@
+'use strict';
 $(function() {
     $('#submit_button').click(function() {
-        var useAsync = $('#use_async').prop('checked');
         var query = $('#query').val();
-        if (useAsync) {
-            makeAsynchronousRequest(query);
-        } else {
-            makeSynchronousRequest(query);
+        if (query) {
+            getAggregatedResults(query);
         }
 
 
     });
 
-    function makeSynchronousRequest(query) {
-
-        if (query) {
-            cleanAllSearchResults();
-            showAllLoadingWheels();
+    function getAggregatedResults(query) {
+            $('#query_results').empty();
+            $('#loading_wheel').show();
             $.ajax({
                 type: 'GET',
                 url: '/aggregated_query/' + query + '/',
                 contentType: 'application/json',
                 success: function(data) {
-                    var googleResults = Handlebars.compile($('#google_template').html())(data);
-                    var yahooResults = Handlebars.compile($('#bing_template').html())(data);
-                    var bingResults = Handlebars.compile($('#yahoo_template').html())(data);
-                    
-                    $('#result_google').html(googleResults);
-                    $('#result_bing').html(bingResults);
-                    $('#result_yahoo').html(yahooResults);
-                    
+                    var googleResults = Handlebars.compile($('#query_template').html())(data);                    
+                    $('#query_results').html(googleResults);
                 },
                 failure: function(errorMsg) {
-                    displayResult(JSON.stringify(errorMsg));
-
+                    $('#query_results').html(JSON.stringify(errorMsg));
                 }
-
             }).always(function() {
-                hideAllLoadingWheels();
+                $('#loading_wheel').hide();
             });
-        }
+        
     }
 
-    function hideAllLoadingWheels() {
-        hideLoadingWheel('google');
-        hideLoadingWheel('yahoo');
-        hideLoadingWheel('bing');
-    }
-
-    function showAllLoadingWheels() {
-        showLoadingWheel('google');
-        showLoadingWheel('yahoo');
-        showLoadingWheel('bing');
-    }
-
-    function cleanAllSearchResults() {
-        cleanPreviousSearchResult('google');
-        cleanPreviousSearchResult('yahoo');
-        cleanPreviousSearchResult('bing');
-    }
-
-    //sends 3 asyncrhnous requests to the server they dont all need to execute correctly
-    //in order to display data on the screen
-    function makeAsynchronousRequest(query) {
-        //they run Asynchronously so our user gets some information back
-        runSearch('google', query);
-        runSearch('bing', query);
-        runSearch('yahoo', query);
-    }
-
-    function runSearch(searchEngine, query) {
-        if (query) {
-            cleanPreviousSearchResult(searchEngine);
-            showLoadingWheel(searchEngine);
-            $.ajax({
-                type: 'GET',
-                url: '/query/' + searchEngine + '/',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: {
-                    'query': query
-                },
-                success: function(data) {
-                    displayResult(searchEngine, data.resp);
-                    //EACH Page needs special formatting
-                    if (window['format_data_' + searchEngine]) {
-                        window['format_data_' + searchEngine]();
-                    }
-                },
-                failure: function(errorMsg) {
-                    displayResult(JSON.stringify(errorMsg));
-
-                }
-
-            }).always(function() {
-                hideLoadingWheel(searchEngine);
-            });
-        }
-    }
-    //helper functions    
-    function hideLoadingWheel(searchEngine) {
-        $('#loading_wheel_' + searchEngine).hide();
-
-    }
-
-    function showLoadingWheel(searchEngine) {
-        $('#loading_wheel_' + searchEngine).show();
-
-    }
-
-    function cleanPreviousSearchResult(searchEngine) {
-        hideLoadingWheel(searchEngine);
-        $('#result_' + searchEngine).empty();
-
-    }
-
-    function displayResult(searchEngine, message) {
-        $('#result_' + searchEngine).html(message);
-    }
-
-    //format the data from the specific provider to clean 
-    // up the HTML that it sends back
-
-    window.format_data_bing = function() {
-        //convert from using ordered lists to unordered list (gets rid of the numbers)
-        $('#result_bing ol').replaceWith(function() {
-            return $('<ul />', {
-                html: $(this).html()
-            });
-        });
-
-        //remove pagination at bottom 
-        $('.b_pag').remove();
-
-    };
-
-    window.format_data_yahoo = function() {
-        //need to move the data-src attr to src for images on yahoo
-        //this is so that we can display the image results from yahoo
-        $('#result_yahoo').each(function() {
-            $(this).find('img').each(function() {
-                var datasource = $(this).attr('data-src');
-                if (datasource) {
-                    $(this).attr('src', datasource);
-                }
-            })
-        });
-
-        //convert from using ordered lists to uniform list
-        $('#result_yahoo ol').replaceWith(function() {
-            return $('<ul />', {
-                html: $(this).html()
-            });
-        });
-        //makes the images display inline 
-        //rather than horizontally
-        //also added a little padding to look clearner
-        $('#dd_image ol').replaceWith(function() {
-            return $('<ul />', {
-                html: $(this).html()
-            });
-        });
-        $('#dd_image li').replaceWith(function() {
-            return $('<span />', {
-                html: $(this).html(),
-                style: "padding:5px;"
-            });
-        });
-
-    };
+    
+   
 });
--- server.js
+++ server.js
@@ -11,10 +11,50 @@ var path = require('path'),
 //local deps 
 var processingFunctions = require('./processing_functions');
 
+var app = express();
+
+// file server for everything in the publc directory 
+app.get('/', function(req, resp) {
+    resp.sendfile(path.resolve('./public/index.html'));
+});
+app.use(express.static(path.resolve('./public')));
 
 
+app.get('/aggregated_query/:query', function(req, resp) {
+    var query = req.param('query');
 
-// constants
+    //process all requests in parallel and then send response when all is processed
+    async.parallel({
+        Google: function(callback) {
+            //use google's api's to generate results 
+            processSearchRequest(generateQueryURL(query, 'google'), processingFunctions.google, callback);
+        },
+        Bing: function(callback) {
+            processSearchRequest(generateQueryURL(query, 'bing'), processingFunctions.bing, callback);
+
+        },
+        Yahoo: function(callback) {
+            processSearchRequest(generateQueryURL(query, 'yahoo'), processingFunctions.yahoo, callback);
+        },
+    }, function(error, results) {
+        if (error) {
+            resp.status(200).send({
+                error: 'Error Handling Request: ' + error
+            });
+        } else {
+            var serviceResponse = {
+                'searchProvider': results,
+                'query': query
+            }
+            resp.status(200).send(serviceResponse);
+        }
+
+    });
+
+
+});
+
+
 
 var queryUrls = {
     'bing': 'http://www.bing.com/search?q=',
@@ -26,113 +66,6 @@ function generateQueryURL(query, searchEngine) {
     return queryUrls[searchEngine] + query;
 }
 
-
-
-var app = express();
-
-// file server for everything in the publc directory 
-app.get('/', function(req, resp) {
-    resp.sendfile(path.resolve('./public/index.html'));
-});
-app.use(express.static(path.resolve('./public')));
-
-
-//Middle man API for search engines 
-app.get('/query/:searchEngine', function(req, resp) {
-    var searchQuery = req.query.query;
-    var searchEngine = req.param('searchEngine');
-
-    if ((searchEngine !== 'yahoo') &&
-        (searchEngine !== 'google') &&
-        (searchEngine !== 'bing')) {
-        resp.status(400).send({
-            error: 'Invalid Search Engine: Supports only google, yahoo and bing'
-        });
-    }
-    if (searchQuery === undefined) {
-        resp.status(400).send({
-            error: 'Invalid Request: Must provide search query'
-        });
-    }
-
-    var searchString;
-    var searchSelector;
-    if (searchEngine === 'yahoo') {
-        searchString = 'https://search.yahoo.com/search?q=';
-        searchSelector = '#main [start=1]';
-    } else if (searchEngine === 'google') {
-        searchString = 'https://www.google.com/search?q=';
-        searchSelector = '.g';
-    } else if (searchEngine === 'bing') {
-        searchString = 'http://www.bing.com/search?q=';
-        searchSelector = '#b_results';
-    }
-
-    request.get(searchString + searchQuery, function(error, response, body) {
-        if (error) {
-            resp.status(500).send({
-                error: 'Something went wrong getting request from ' + searchEngine + ' ' + error
-            });
-        }
-        var handler = new htmlparser.DefaultHandler(function(error, dom) {
-            if (error) {
-                resp.status(500).send({
-                    error: 'something went wrong with parsing response from ' + searchEngine + ' ' + error
-                });
-            } else {
-
-                var queries = parserToHtml(select(dom, searchSelector));
-
-                resp.status(200).send({
-                    resp: queries
-                });
-            }
-        });
-
-        var parser = new htmlparser.Parser(handler);
-        parser.parseComplete(body);
-
-
-    });
-
-});
-
-
-
-
-
-
-
-app.get('/aggregated_query/:query', function(req, resp) {
-    var query = req.param('query');
-
-    //process all requests in parallel and then send response when all is processed
-    async.parallel({
-        google: function(callback) {
-            //use google's api's to generate results 
-            processSearchRequest(generateQueryURL(query, 'google'), processingFunctions.google, callback);
-        },
-        bing: function(callback) {
-            processSearchRequest(generateQueryURL(query, 'bing'), processingFunctions.bing, callback);
-
-        },
-        yahoo: function(callback) {
-            processSearchRequest(generateQueryURL(query, 'yahoo'), processingFunctions.yahoo, callback);
-        },
-    }, function(error, results) {
-        if (error) {
-            resp.status(200).send({
-                error: 'Error Handling Request: ' + error
-            });
-        } else {
-            results.query = query;
-            resp.status(200).send(results);
-        }
-
-    });
-
-
-});
 
 //Keep things DRY by writing generic processes function
 //specialized handling is needed on a case by case basis
